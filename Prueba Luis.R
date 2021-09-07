1f38b2a942125aed404f85be6fa025a4473e0098
Zambiazzo <yanina.zambiazzo@gmail.com>
2021-09-06 22:10:04 -0300

    Prueba 100000000

--- Prueba Luis.R
+++ Prueba Luis.R
@@ -1,7 +1,7 @@
 Hola 
 Holaaaaa
 library(usethis)
-credentials::set_github_pat("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")
+credentials::set_github_pat("")
 use_git_config(user.name = "Zambiazzo", user.email = "yanina.zambiazzo@gmail.com")
 
 update.packages(ask = FALSE, checkBuilt = TRUE)
